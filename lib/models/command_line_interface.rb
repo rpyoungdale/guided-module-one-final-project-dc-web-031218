43994c64a2504a3cc3b642c5327f6a2341f79019
rpyoungdale <youngdale95@gmail.com>
2018-03-29 13:37:03 -0400

    Ryan added last four methods

--- lib/models/command_line_interface.rb
+++ lib/models/command_line_interface.rb
@@ -11,13 +11,13 @@ class CommandLineInterface
     puts "What do you want to do?"
     puts "-------------------------"
     puts "Please Select an Option:"
-    puts "1. Enter New Game"
-    puts "2. Total Wins"
-    puts "3. Total Wins By Sport"
-    puts "4. Total Games Played"
-    puts "5. Total Games Played By Sport"
-    puts "6. Find A Certain Score"
-    puts "7. Delete Game"
+    puts "  1. Enter New Game"
+    puts "  2. Total Wins"
+    puts "  3. Total Wins By Sport"
+    puts "  4. Total Games Played"
+    puts "  5. Total Games Played By Sport"
+    puts "  6. Find A Certain Score"
+    puts "  7. Delete Game"
 
     user_input = gets.chomp
 
@@ -31,13 +31,13 @@ class CommandLineInterface
     when "3"
       total_wins_by_sport
     when "4"
-
+      total_games_played
     when "5"
       total_games_played_by_sport
     when "6"
-
+      find_score
     when "7"
-
+      delete_game
     end
   end
 
@@ -54,14 +54,68 @@ class CommandLineInterface
   def total_wins_by_sport
   end
 
-  def
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  def total_games_played
+    @current_user.user_total_rounds_played
   end
-
-
 
   def total_games_played_by_sport
     puts "Which sport?"
     sports_input = gets_input
-    user_rounds_played_by_sport(sport_input)
+    @current_user.user_rounds_played_by_sport(sport_input)
+  end
+
+  def find_score
+    puts "What date did you play this game? Enter: mm/dd/yy"
+    date_input = gets_input
+    @current_user.find_round_score(date_input)
+  end
+
+  def delete_game
+    puts "What date did you play this game? Enter: mm/dd/yy"
+    date_input = gets_input
+    @current_user.delete_round(date_input)
   end
 end
